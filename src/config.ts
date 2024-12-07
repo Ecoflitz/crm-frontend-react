21d74c4b99f177a4d9422894d8efeed166658727
Ecoflitz <admin@ecoflitz.co.uk>
2024-12-07 00:00:00 +0000

    updated port

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 export const hostApi = 'https://crm.ecoflitz.com';
 // process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://crm.ecoflitz.com';
 export const portApi = process.env.NODE_ENV === 'development' ? 8080 : '';
-export const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
+// export const baseURLApi = `${hostApi}${portApi ? `:${portApi}` : ``}/api`;
+
+export const baseURLApi = `${hostApi}/api`;
 
 export const localStorageDarkModeKey = 'darkMode';
 
